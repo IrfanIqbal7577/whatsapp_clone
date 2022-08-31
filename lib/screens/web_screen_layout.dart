dbe65c8f482f046cfa1bb3947ee265c8ca794cfa
Muaaz Shoaib <muaazshoaib1670@gmail.com>
2022-08-31 13:39:04 +0500

    Displaying Web Contacts List

--- lib/screens/web_screen_layout.dart
+++ lib/screens/web_screen_layout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/widgets/contact_list.dart';
 
 // WebScreenLayout: User defined class.
 // StatelessWidget: A widget that does not require mutable state.
@@ -9,13 +10,34 @@ class WebScreenLayout extends StatelessWidget {
   // build(BuildContext context) → Widget: Describes the part of the user interface represented by this widget.
   Widget build(BuildContext context) {
     // Scaffold: Implements the basic Material Design visual layout structure.
-    return const Scaffold(
+    return Scaffold(
       // body: The primary content of the scaffold.
       // Center: A widget that centers its child within itself.
-      body: Center(
-        // child: The widget below this widget in the tree.
-        // Text: A run of text with a single style.
-        child: Text('This is web'),
+      body: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: const [
+                  // Web Profile Bar
+                  // Web Search Bar
+                  ContactsList(),
+                ],
+              ),
+            ),
+          ),
+          // Web Screen
+          Container(
+            width: MediaQuery.of(context).size.width * 0.75,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/backgroundImage.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/widgets/contact_list.dart
+++ lib/widgets/contact_list.dart
@@ -11,59 +11,58 @@ class ContactsList extends StatelessWidget {
       padding: const EdgeInsets.only(
         top: 10,
       ),
-      child: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              shrinkWrap: true,
-              itemCount: info.length,
-              itemBuilder: (BuildContext context, int index) {
-                return InkWell(
-                  onTap: () {},
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                      bottom: 8.0,
+      child: ListView.builder(
+        shrinkWrap: true,
+        itemCount: info.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Column(
+            children: [
+              InkWell(
+                onTap: () {},
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    bottom: 8.0,
+                  ),
+                  child: ListTile(
+                    title: Text(
+                      info[index]['name'].toString(),
+                      style: const TextStyle(
+                        fontSize: 18,
+                      ),
                     ),
-                    child: ListTile(
-                      title: Text(
-                        info[index]['name'].toString(),
+                    subtitle: Padding(
+                      padding: const EdgeInsets.only(
+                        top: 6.0,
+                      ),
+                      child: Text(
+                        info[index]['message'].toString(),
                         style: const TextStyle(
-                          fontSize: 18,
+                          fontSize: 15,
                         ),
                       ),
-                      subtitle: Padding(
-                        padding: const EdgeInsets.only(
-                          top: 6.0,
-                        ),
-                        child: Text(
-                          info[index]['message'].toString(),
-                          style: const TextStyle(
-                            fontSize: 15,
-                          ),
-                        ),
-                      ),
-                      leading: CircleAvatar(
-                        backgroundImage:
-                            NetworkImage(info[index]['profilePic'].toString()),
-                      ),
-                      trailing: Text(
-                        info[index]['time'].toString(),
-                        style: const TextStyle(
-                          fontSize: 13,
-                          color: Colors.grey,
-                        ),
+                    ),
+                    leading: CircleAvatar(
+                      backgroundImage:
+                          NetworkImage(info[index]['profilePic'].toString()),
+                      radius: 30,
+                    ),
+                    trailing: Text(
+                      info[index]['time'].toString(),
+                      style: const TextStyle(
+                        fontSize: 13,
+                        color: Colors.grey,
                       ),
                     ),
                   ),
-                );
-              },
-            ),
-          ),
-          const Divider(
-            color: dividerColor,
-            indent: 85,
-          ),
-        ],
+                ),
+              ),
+              const Divider(
+                color: dividerColor,
+                indent: 85,
+              ),
+            ],
+          );
+        },
       ),
     );
   }
