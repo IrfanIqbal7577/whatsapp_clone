a1150ef3e9723fcc3196e40349bb62021810b9e0
Muaaz Shoaib <muaazshoaib1670@gmail.com>
2022-09-06 21:27:00 +0500

    OTP Screen UI

--- lib/features/landing/auth/screens/login_screen.dart
+++ lib/features/landing/auth/screens/login_screen.dart
@@ -2,6 +2,7 @@ import 'package:country_picker/country_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:whatsapp_clone/colors.dart';
+import 'package:whatsapp_clone/common/utils/utils.dart';
 import 'package:whatsapp_clone/common/widgets/custom_button.dart';
 import 'package:whatsapp_clone/features/landing/auth/controller/auth_controller.dart';
 
@@ -40,6 +41,8 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
       ref
           .read(authControllerProvider)
           .signInWithPhone(context, '+${country!.phoneCode}$phoneNumber');
+    } else {
+      showSnackBar(context: context, content: 'Fill out all the feilds.');
     }
   }
 
@@ -54,39 +57,48 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(18.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
+        child: ListView(
           children: [
-            const Text(
-              'WhatsApp will need to verify your phone number.',
-            ),
-            const SizedBox(height: 10),
-            TextButton(
-              onPressed: pickCountry,
-              child: const Text('Pick Country'),
-            ),
-            const SizedBox(height: 5),
-            Row(
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                if (country != null) Text('+${country!.phoneCode}'),
-                const SizedBox(width: 10),
-                SizedBox(
-                  width: size.width * 0.7,
-                  child: TextField(
-                    controller: phoneController,
-                    decoration: const InputDecoration(
-                      hintText: 'phone number',
+                const Text(
+                  'WhatsApp will need to verify your phone number.',
+                ),
+                const SizedBox(height: 10),
+                TextButton(
+                  onPressed: pickCountry,
+                  child: const Text('Pick Country'),
+                ),
+                const SizedBox(height: 5),
+                Row(
+                  children: [
+                    if (country != null) Text('+${country!.phoneCode}'),
+                    const SizedBox(width: 10),
+                    SizedBox(
+                      width: size.width * 0.7,
+                      child: TextField(
+                        controller: phoneController,
+                        decoration: const InputDecoration(
+                          hintText: 'phone number',
+                        ),
+                        keyboardType: TextInputType.number,
+                        onChanged: (value) {},
+                      ),
                     ),
+                  ],
+                ),
+                SizedBox(
+                  height: size.height * 0.6,
+                ),
+                SizedBox(
+                  width: 90,
+                  child: CustomButton(
+                    text: 'NEXT',
+                    onPressed: sentPhoneNumber,
                   ),
                 ),
               ],
-            ),
-            SizedBox(
-              height: size.height * 0.6,
-            ),
-            SizedBox(
-              width: 90,
-              child: CustomButton(text: 'NEXT', onPressed: () {}),
             ),
           ],
         ),
